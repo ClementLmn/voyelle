c465f84f25b4ed1445365f0dd44600f7c1ac90e0
ClementLmn <clementlemoine.cl@gmail.com>
2017-09-12 12:54:06 +0200

    canvas de bg + detection en cercle

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,22 @@ $(function(){
     var poem = document.querySelector('#poem'), text = poem.textContent.split(''), bigChar = $('.big-char'), letters, words;
     var voyelles = ['a', 'A', 'e', 'E', 'i', 'I', 'O', 'o', 'u', 'U'];
     var phaseJump = 360 / text.length;
-    var rectSizeX, rectSizeY;
+    var rectSizeX, rectSizeY, r;
+    var radius = 0, opacity = 1;
+    var iCircle = 0;
     var doneWords = [];
+    var canvas = $('#canvas');
+    var ctx = document.getElementById('canvas').getContext('2d');
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
 
     function getRandom(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    function easeOutCubic(currentIteration, startValue, changeInValue, totalIterations) {
+        return changeInValue * (Math.pow(currentIteration / totalIterations - 1, 3) + 1) + startValue;
+    }
 
     function wordCheck(){
         words.each(function(i){
@@ -32,11 +41,12 @@ $(function(){
                 });
                 if(numVoy == vDone){
                     $(this).data('complete', true);
-                    $(this).find(':not(.voy)').each(function(i){
-                        TweenLite.fromTo($(this), 0.3, {y: -3}, {ease: Power2.easeInOut, y: 0, delay : i * 0.05});
-                    });
                     $(this).find('.voy').each(function(i){
                         $(this).css('color', '');
+                    });
+                    $(this).find(':not(.voy)').each(function(i){
+                        TweenLite.fromTo($(this), 0.3, {y: -3}, {ease: Power2.easeInOut, y: 0, delay : i * 0.05});
+                        TweenLite.to($(this), 0.3, {css:{autoAlpha: 1}, delay : i * 0.05});
                     });
                     doneWords.push($(this));
                 }
@@ -45,14 +55,17 @@ $(function(){
     }
 
     function searchLetter(start, toColor){
+        var x = start.data('offsetLeft');
+        var y = start.data('offsetTop');
+        r = getRandom(100, 250);
+        drawCircle(x,y,toColor);
         var d = 0;
-        rectSizeX = getRandom(100, 300);
-        rectSizeY = getRandom(100, 300);
         letters.each(function(i){
             if(start.data('letter') == $(this).data('letter')){
-                if(($(this).data('offsetTop') > start.data('offsetTop') - rectSizeY && $(this).data('offsetTop') < start.data('offsetTop') + rectSizeY) && ($(this).data('offsetLeft') > start.data('offsetLeft') - rectSizeX && $(this).data('offsetLeft') < start.data('offsetLeft') + rectSizeX) && $(this).data('hidden') ){
+                // (x1 - x2)^2 + (y1 - y2)^2 <= r^2 TRIGONOMETRIE BITCH
+                if(Math.pow(($(this).data('offsetLeft') - start.data('offsetLeft')), 2) + Math.pow(($(this).data('offsetTop') - start.data('offsetTop')), 2) < Math.pow(r, 2) && $(this).data('hidden')){
                     $(this).data('hidden', false);
-                    TweenLite.to($(this), 0.3, {css:{color : toColor}, delay : d});
+                    TweenLite.to($(this), 0.3, {css:{color : toColor, autoAlpha: 1}, delay : d});
                     TweenLite.fromTo($(this), 0.3, {y: -10}, {ease: Power2.easeInOut, y: 0, delay : d});
                     d += 0.05;
                     $(this).on('click', function(e){
@@ -62,7 +75,7 @@ $(function(){
                 }
             }
         });
-        setTimeout(wordCheck, d * 1000 + 800);
+        setTimeout(wordCheck, d * 1000 + 600);
     }
 
     function initText(){
@@ -91,6 +104,39 @@ $(function(){
         });
     }
 
+
+    // Petit canvas fait avec les fesses
+    function drawCircle(x,y,color){
+        var coordX = x;
+        var coordY = y;
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        if(radius < r){
+            ctx.beginPath();
+            ctx.globalAlpha = opacity;
+            ctx.arc(coordX, coordY, radius, 0, 2 * Math.PI, false);
+            ctx.lineWidth = 5;
+            ctx.fillStyle = color;
+            ctx.fill();
+            radius = easeOutCubic(iCircle, 0, r, 50);
+            opacity = easeOutCubic(iCircle, 1, -1, 50)
+            iCircle++;
+            requestAnimationFrame(function(){
+                drawCircle(x,y);
+            });
+        }else{
+            opacity = 1;
+            radius = 0;
+            iCircle = 0;
+        }
+    }
+
+    function attentionOndeDeChoc(start, color){
+        var x = start.data('offsetLeft');
+        var y = start.data('offsetTop');
+        drawCircle(x,y,color);
+        searchLetter(start, color);
+    }
+
     function buttonColor() {
         bigChar.each(function( i ) {
             $(this).css('color', 'hsl(' + (angle + i* 360 / bigChar.length) + ', 55%, 70%)');
@@ -111,11 +157,16 @@ $(function(){
     buttonColor();
     wheee();
 
+    // canvas.on('click', function(e){
+    //     console.log('click1');
+    //     attentionOndeDeChoc();
+    // });
+
     bigChar.each(function(){
         $(this).on('click', function(e){
             var start = $(this);
             searchLetter(start, $(this).data('color'));
-            $(this).removeClass('big-char').off( "click");
+            //$(this).removeClass('big-char').off( "click");
         });
     });
 
